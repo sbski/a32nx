4129262257d8ab1b1b1b0d62b312c6976aec5369
BBK <22713769+BlueberryKing@users.noreply.github.com>
2022-12-10 22:59:31 +0100

    Refactor geometric path building

--- src/fmgc/src/guidance/vnav/descent/GeometricPathBuilder.ts
+++ src/fmgc/src/guidance/vnav/descent/GeometricPathBuilder.ts
@@ -21,15 +21,18 @@ export class GeometricPathBuilder {
     }
 
     executeGeometricSegments(sequence: TemporaryCheckpointSequence, segments: PlannedGeometricSegment[], speedConstraints: MaxSpeedConstraint[], windProfile: HeadwindProfile) {
+        const accelerationTargets = this.buildAccelerationTargets(sequence.lastCheckpoint, segments, speedConstraints);
+        const lastTarget: AccelerationTarget = null;
+
         for (const segment of segments) {
             const currentSegmentSequence = new TemporaryCheckpointSequence(sequence.lastCheckpoint);
 
-            if (!this.executeGeometricSegment(currentSegmentSequence, segment, speedConstraints, windProfile, false)) {
-                if (!this.executeGeometricSegment(currentSegmentSequence, segment, speedConstraints, windProfile, true)) {
+            if (!this.executeGeometricSegment(currentSegmentSequence, segment, accelerationTargets, windProfile, false, lastTarget)) {
+                if (!this.executeGeometricSegment(currentSegmentSequence, segment, accelerationTargets, windProfile, true, lastTarget)) {
                     // Marking the segment as too steep, so that we ignore speed constraints on the next try and just fly at the maximum possible gradient
                     segment.isTooSteep = true;
 
-                    this.executeGeometricSegment(currentSegmentSequence, segment, speedConstraints, windProfile, true);
+                    this.executeGeometricSegment(currentSegmentSequence, segment, accelerationTargets, windProfile, true, lastTarget);
 
                     sequence.push(...currentSegmentSequence.get());
                     sequence.copyLastCheckpoint({
@@ -45,56 +48,31 @@ export class GeometricPathBuilder {
         }
     }
 
-    private executeGeometricSegment(
-        sequence: TemporaryCheckpointSequence, segment: PlannedGeometricSegment, speedConstraints: MaxSpeedConstraint[], windProfile: HeadwindProfile, useSpeedbrakes: boolean,
-    ) {
-        const { managedDescentSpeed, managedDescentSpeedMach, descentSpeedLimit } = this.observer.get();
+    private executeGeometricSegment(sequence: TemporaryCheckpointSequence,
+        segment: PlannedGeometricSegment,
+        accelerationTargets: AccelerationTarget[],
+        windProfile: HeadwindProfile,
+        useSpeedbrakes: boolean,
+        lastTarget: AccelerationTarget) {
+        const { managedDescentSpeedMach } = this.observer.get();
         this.flightPathAngleStrategy.flightPathAngle = MathUtils.RADIANS_TO_DEGREES * Math.atan(segment.gradient / 6076.12);
 
-        let econSpeed = managedDescentSpeed;
-
-        // We treat the speed limit like a constraint if we cross it on this segment
-        const isSpeedLimitValid = Number.isFinite(descentSpeedLimit.speed) && Number.isFinite(descentSpeedLimit.underAltitude);
-        let wasBelowSpeedLimitAlt = sequence.lastCheckpoint.altitude <= descentSpeedLimit.underAltitude;
-        if (isSpeedLimitValid && wasBelowSpeedLimitAlt) {
-            if (segment.end.altitude > descentSpeedLimit.underAltitude) {
-                const distanceToSpeedLimit = (descentSpeedLimit.underAltitude - sequence.lastCheckpoint.altitude) / segment.gradient;
-
-                speedConstraints.push({
-                    distanceFromStart: sequence.lastCheckpoint.distanceFromStart + distanceToSpeedLimit,
-                    maxSpeed: descentSpeedLimit.speed,
-                });
-
-                speedConstraints.sort((a, b) => a.distanceFromStart - b.distanceFromStart);
-            } else {
-                econSpeed = Math.min(econSpeed, descentSpeedLimit.speed);
-            }
-        }
-
-        for (let i = speedConstraints.length - 1; i >= 0 && !segment.isTooSteep; i--) {
-            const speedConstraint = speedConstraints[i];
+        for (let i = 0; i < accelerationTargets.length && !segment.isTooSteep; i++) {
+            const accelerationTarget = accelerationTargets[i];
             const maxDistance = segment.end.distanceFromStart - sequence.lastCheckpoint.distanceFromStart;
-            const isBelowSpeedLimitAlt = isSpeedLimitValid && sequence.lastCheckpoint.altitude <= descentSpeedLimit.underAltitude;
 
-            if (speedConstraint.distanceFromStart > sequence.lastCheckpoint.distanceFromStart || maxDistance >= 0) {
+            if (accelerationTarget.distanceFromStart > sequence.lastCheckpoint.distanceFromStart || maxDistance >= 0) {
                 continue;
             }
 
-            let maxSpeed = econSpeed;
-            for (let j = 0; j <= i; j++) {
-                const sc = speedConstraints[j];
-
-                if (sc.distanceFromStart > sequence.lastCheckpoint.distanceFromStart) {
-                    break;
-                }
-
-                maxSpeed = Math.min(maxSpeed, sc.maxSpeed);
+            if (lastTarget == null) {
+                lastTarget = accelerationTarget;
             }
 
             // Decelerate to speed target
             const decelerationStep = this.flightPathAngleStrategy.predictToSpeed(
                 sequence.lastCheckpoint.altitude,
-                maxSpeed,
+                accelerationTarget.speed,
                 sequence.lastCheckpoint.speed,
                 managedDescentSpeedMach,
                 sequence.lastCheckpoint.remainingFuelOnBoard,
@@ -121,36 +99,36 @@ export class GeometricPathBuilder {
             }
 
             // These are negative distances
+            // Check if the deceleration takes us past the segment bounds.
             if (decelerationStep.distanceTraveled < Math.min(maxDistance, 0)) {
                 const scaling = maxDistance / decelerationStep.distanceTraveled;
 
                 this.scaleStepBasedOnLastCheckpoint(sequence.lastCheckpoint, decelerationStep, scaling);
                 sequence.addCheckpointFromStep(decelerationStep, VerticalCheckpointReason.SpeedConstraint);
-            } else if (Math.max(speedConstraint.distanceFromStart, segment.end.distanceFromStart) - sequence.lastCheckpoint.distanceFromStart < 0) {
-                const didCrossoverSpeedLimitAltitude = isSpeedLimitValid && !isBelowSpeedLimitAlt && wasBelowSpeedLimitAlt;
-                const checkpointReason = didCrossoverSpeedLimitAltitude ? VerticalCheckpointReason.StartDecelerationToLimit : VerticalCheckpointReason.StartDecelerationToConstraint;
-
-                sequence.addDecelerationCheckpointFromStep(decelerationStep, checkpointReason, speedConstraint.maxSpeed);
+            } else if (Math.max(accelerationTarget.distanceFromStart, segment.end.distanceFromStart) < sequence.lastCheckpoint.distanceFromStart + decelerationStep.distanceTraveled) {
+                if (decelerationStep.distanceTraveled < 0) {
+                    const decelerationReason = lastTarget.isSpeedLimit ? VerticalCheckpointReason.StartDecelerationToLimit : VerticalCheckpointReason.StartDecelerationToConstraint;
+                    sequence.addDecelerationCheckpointFromStep(decelerationStep, decelerationReason, lastTarget.speed);
+                }
 
                 // Fly to constraint
                 const stepToConstraint = this.flightPathAngleStrategy.predictToDistance(
                     sequence.lastCheckpoint.altitude,
-                    Math.max(speedConstraint.distanceFromStart, segment.end.distanceFromStart) - sequence.lastCheckpoint.distanceFromStart,
+                    Math.max(accelerationTarget.distanceFromStart, segment.end.distanceFromStart) - sequence.lastCheckpoint.distanceFromStart,
                     sequence.lastCheckpoint.speed,
                     managedDescentSpeedMach,
                     sequence.lastCheckpoint.remainingFuelOnBoard,
                     windProfile.getHeadwindComponent(sequence.lastCheckpoint.distanceFromStart, sequence.lastCheckpoint.altitude),
                 );
 
-                sequence.addCheckpointFromStep(stepToConstraint, VerticalCheckpointReason.SpeedConstraint);
+                const checkpointReason = accelerationTarget.isSpeedLimit ? VerticalCheckpointReason.CrossingDescentSpeedLimit : VerticalCheckpointReason.SpeedConstraint;
+                sequence.addCheckpointFromStep(stepToConstraint, checkpointReason);
+                lastTarget = accelerationTarget;
             }
-
-            wasBelowSpeedLimitAlt = isBelowSpeedLimitAlt;
         }
 
         if (segment.end.distanceFromStart - sequence.lastCheckpoint.distanceFromStart < 0) {
             // Fly to end of segment
-            // TODO: It's possible we are not at the econSpeed yet. If so, we should accelerate to it
             const stepToEndOfSegment = this.flightPathAngleStrategy.predictToDistance(
                 sequence.lastCheckpoint.altitude,
                 segment.end.distanceFromStart - sequence.lastCheckpoint.distanceFromStart,
@@ -173,4 +151,70 @@ export class GeometricPathBuilder {
         step.finalAltitude = (1 - scaling) * lastCheckpoint.altitude + scaling * step.finalAltitude;
         step.speed = (1 - scaling) * lastCheckpoint.speed + scaling * step.speed;
     }
+
+    private buildAccelerationTargets(startingPoint: VerticalCheckpoint, segments: PlannedGeometricSegment[], speedConstraints: MaxSpeedConstraint[]): AccelerationTarget[] {
+        const { managedDescentSpeed, descentSpeedLimit } = this.observer.get();
+
+        let shouldAddSpeedLimit = true;
+        const distanceAtSpeedLimitCrossing = this.findDistanceAtAltitude(descentSpeedLimit.underAltitude, startingPoint, segments);
+        const speedLimitTarget: AccelerationTarget = {
+            distanceFromStart: distanceAtSpeedLimitCrossing,
+            speed: descentSpeedLimit.speed,
+            isSpeedLimit: true,
+        };
+
+        const targets: AccelerationTarget[] = [];
+        for (let i = speedConstraints.length - 1; i >= 0; i--) {
+            const sc = speedConstraints[i];
+
+            if (shouldAddSpeedLimit && sc.distanceFromStart < distanceAtSpeedLimitCrossing) {
+                targets.push(speedLimitTarget);
+                shouldAddSpeedLimit = false;
+            }
+
+            const target = {
+                distanceFromStart: sc.distanceFromStart,
+                speed: sc.maxSpeed,
+                isSpeedLimit: false,
+            };
+
+            targets.push(target);
+        }
+
+        targets.push({
+            distanceFromStart: -Infinity,
+            speed: managedDescentSpeed,
+            isSpeedLimit: false,
+        });
+
+        // Propagate speed constraints forwards
+        targets.reduceRight((maxSpeed, target) => {
+            maxSpeed = Math.min(maxSpeed, target.speed);
+            target.speed = maxSpeed;
+
+            return maxSpeed;
+        }, Infinity);
+
+        return targets;
+    }
+
+    private findDistanceAtAltitude(altitude: number, startingPoint: VerticalCheckpoint, segments: PlannedGeometricSegment[]): NauticalMiles | null {
+        if (startingPoint.altitude > altitude) {
+            return null;
+        }
+
+        for (const segment of segments) {
+            if (altitude <= segment.end.altitude) {
+                return segment.end.distanceFromStart + (altitude - segment.end.altitude) / segment.gradient;
+            }
+        }
+
+        return null;
+    }
+}
+
+type AccelerationTarget = {
+    distanceFromStart: NauticalMiles;
+    speed: number;
+    isSpeedLimit: boolean;
 }
