4b13de74009d1a8aed0a883c161fa19f01d2d3f8
Sven Czarnian <devel@svcz.de>
2023-01-13 20:25:11 +0100

    fix linter errors

--- src/systems/a380_systems/src/lib.rs
+++ src/systems/a380_systems/src/lib.rs
@@ -23,10 +23,7 @@ use power_consumption::A380PowerConsumption;
 use std::vec::Vec;
 use systems::enhanced_gpwc::EnhancedGPWC;
 use systems::simulation::InitContext;
-use uom::si::{
-    f64::Length,
-    length::nautical_mile,
-};
+use uom::si::{f64::Length, length::nautical_mile};
 
 use systems::{
     apu::{
@@ -129,7 +126,7 @@ impl A380 {
                     Length::new::<nautical_mile>(160.0),
                     Length::new::<nautical_mile>(320.0),
                     Length::new::<nautical_mile>(640.0),
-                ])
+                ]),
             ),
         }
     }
